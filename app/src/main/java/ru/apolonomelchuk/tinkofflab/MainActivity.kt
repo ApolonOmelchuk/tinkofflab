b8de3a29b9732c6e0bc7cb33fd07a1c13a3603b5
ApolonOmelchuk <apolonomelchuk@gmail.com>
2020-08-28 23:16:57 +0300

    OnClick events

--- app/src/main/java/ru/apolonomelchuk/tinkofflab/MainActivity.kt
+++ app/src/main/java/ru/apolonomelchuk/tinkofflab/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.apolonomelchuk.tinkofflab
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -25,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
     }
 
-    fun getNextGif() {
+    fun getNextGif(view: View) {
         val nextGifFromCache = gifsList.getNext()
         val nextGif = if (nextGifFromCache != null) nextGifFromCache as GifWrapper else getNextRandomGif()
         loadGif(nextGif)
@@ -34,7 +35,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun getPrevGif() {
+    fun getPrevGif(view: View) {
         val prevGifFromCache = gifsList.getPrevious() as GifWrapper
         loadGif(prevGifFromCache)
         if (!gifsList.isFirstElement(prevGifFromCache)) {
